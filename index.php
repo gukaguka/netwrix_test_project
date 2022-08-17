be63dd617ca4868dcd9f3791dd1fce6622e0a580
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-08-18 03:04:28 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 	
-
-		echo "test"
-  
-
-
+	include("config.php");
+	$query = $connect->query("SELECT * FROM loc_country");
 ?>
+
+<?php
+while($row = $query->fetcha_assoc() ){
+	
+	echo "<table>
+	<tr>
+		<td>
+		{$row['name]}
+		</td>
+	</tr>
+
+</table>"
+}
+	?>
