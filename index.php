e2969ab81cc57085f4ea03bcc38a54f4dc775093
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-08-18 02:54:44 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,8 @@
 	
 	include("config.php")
 
-	
-  	$query = "SELECT * FROM loc_country"
-  	$data = mysqli_query($connect, $query);
-
-  	while($row = mysqli_fetch_assoc($data) ){
-
-  		echo $row;
-  	}
+		test
+  
 
 
 ?>
