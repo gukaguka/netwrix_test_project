aa3b9e03c15c308e80de5fe40ab983be8704f902
gukaguka <68656472+gukaguka@users.noreply.github.com>
2022-08-18 02:48:46 +0400

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
   $db_password ="ad94e10e";
   $db_name ="heroku_b282da8a31f51a7";
 
-  $connect = mysql_connect($db_host, $db_user, $db_password, $db_name) or die("database connection error");
+  $connect = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die("database connection error");
 
 
 ?>
